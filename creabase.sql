039c0ce4cc8b2149fe434a8533eab1ca74611672
Moarff <emmyesteam@laposte.net>
2019-01-15 11:30:45 +0100

    Outils (css)

--- creabase.sql
+++ creabase.sql
@@ -15,7 +15,8 @@ USE projet_calendrier_ufc;
 create table USERS
 (
 numUser INT(4) PRIMARY KEY NOT NULL,
-pwUser VARCHAR(20) not null
+pwUser VARCHAR(20) NOT NULL,
+nameUser VARCHAR(30) NOT NULL;
 );
 
 -- ============================================================
@@ -42,3 +43,34 @@ constraint fk_link_calendar foreign key (calendar) references CALENDAR(idCalenda
 constraint fk_link_USERS foreign key (USERS) references USERS(numUser)
 );
 
+-- ============================================================
+--   Table : LINK                                             
+-- ============================================================
+
+insert into USERS values
+     (1,'abc', 'jean');
+insert into USERS values
+     (2,'abc', 'pierre');
+insert into USERS values
+     (3,'abc', 'paul');
+insert into USERS values
+     (4,'abc', 'jacques');
+insert into USERS values
+     (5,'abc', 'nicole');
+	 
+	 
+insert into CALENDAR values
+	 (1, 2, '22/05/03');
+insert into CALENDAR values
+	 (2, 2, '22/05/03');
+insert into CALENDAR values
+	 (3, 2, '22/05/03');
+insert into CALENDAR values
+	 (4, 2, '22/05/03');
+create table CALENDAR
+(
+idCalendar INT(4) PRIMARY KEY NOT NULL,
+owner INT(4) NOT NULL,
+dateCreation date NOT NULL,
+constraint fk_calendar foreign key (owner) references USERS(numUser)
+);
